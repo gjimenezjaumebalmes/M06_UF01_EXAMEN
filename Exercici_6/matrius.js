ae6cc8b6d8c1505f4df684137a1f5bd0423f2168
FHP187 <gjimenez@jaumebalmes.net>
2022-11-23 20:26:10 +0100

    Commit exercici 4

---
+++ Exercici_6/matrius.js
@@ -0,0 +1,30 @@
+
+    let rows = document.querySelector("tbody").children
+    let matrix = []
+    for (var i = 0; i < rows.length; i++) {
+    matrix.push(rows[i].children)
+}
+
+    function erase() {
+    for (let i = 0; i < rows.length; i++) {
+    var trList = rows[i].children;
+    for (let j = 0; j < trList.length; j++) {
+    matrix[i][j].style.backgroundColor = "white";
+}
+}
+}
+
+
+    function pintaQuadrant() {
+    erase();
+    for (let i = 0; i < rows.length; i++) {
+    var trList = rows[i].children;
+    var laMeitatesquerra = trList.length / 4;
+    for (let j = 0; j < trList.length; j++) {
+    if (j < laMeitatesquerra) {
+    matrix[i][j].style.backgroundColor = "red";
+}
+}
+}
+
+}
